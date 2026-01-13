142b54f247e6bbbc5a8b5e2f945d6b783146eb1d
Mark <85582966+MarkPalkimas@users.noreply.github.com>
2026-01-13 01:20:35 -0500

    Add ClerkProviderWithRouter component

---
+++ src/ClerkProviderWithRouter.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { ClerkProvider } from '@clerk/clerk-react'
+import { useNavigate } from 'react-router-dom'
+
+export default function ClerkProviderWithRouter({ children }) {
+  const navigate = useNavigate()
+
+  const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+  if (!clerkPubKey) {
+    throw new Error('Missing Publishable Key')
+  }
+
+  return (
+    <ClerkProvider
+      publishableKey={clerkPubKey}
+      navigate={(to) => navigate(to)}
+      signInUrl="/sign-in"
+      signUpUrl="/sign-up"
+      afterSignInUrl="/"
+      afterSignUpUrl="/"
+    >
+      {children}
+    </ClerkProvider>
+  )
+}

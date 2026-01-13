b5ac18fdf703bdbcdf8c29acf53200cf0241c114
Mark <85582966+MarkPalkimas@users.noreply.github.com>
2026-01-13 01:21:07 -0500

    Update SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import { SignUp } from '@clerk/clerk-react'
 
 export default function SignUpPage() {
-    return (
-        <div style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            minHeight: '100vh',
-            background: '#f9fafb'
-        }}>
-            <SignUp routing="path" path="/sign-up" signInUrl="/sign-in" />
-        </div>
-    )
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh',
+        background: '#f9fafb',
+      }}
+    >
+      <SignUp routing="hash" path="/sign-up" signInUrl="/sign-in" />
+    </div>
+  )
 }

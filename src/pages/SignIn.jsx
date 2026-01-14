97f0dc466f17ef77348ac80067a78bbdb2167fd2
Mark <85582966+MarkPalkimas@users.noreply.github.com>
2026-01-14 16:59:32 -0500

    Update SignIn.jsx

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,18 +1,10 @@
-import React from 'react'
-import { SignIn } from '@clerk/clerk-react'
+import React from "react";
+import { SignIn } from "@clerk/clerk-react";
 
 export default function SignInPage() {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        minHeight: '100vh',
-        background: '#f9fafb',
-      }}
-    >
-      <SignIn afterSignInUrl="/" />
+    <div style={{ display:"flex", justifyContent:"center", alignItems:"center", minHeight:"100vh", background:"#f9fafb" }}>
+      <SignIn routing="hash" path="/sign-in" afterSignInUrl="/" />
     </div>
-  )
+  );
 }
